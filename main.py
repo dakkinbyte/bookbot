e565044c588e20d35b0433e2f0a8de13e560394c
Dakkinbyte <fantauzzo94@gmail.com>
2024-04-01 19:37:35 -0400

    New Changes

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("hello world")
+def print_file(file_path):
+        print(file_path)
+
+def read_file(file_path):
+    with open(file_path, 'r') as file:
+        return file.read()
+
+def stringConversion(input_string):
+    words = input_string.split(' ')
+    word_count = len(words)
+    print("Word count: ", word_count)
+
+def main():
+    file_contents = read_file('books/frankenstein.txt')
+    print_file(file_contents)
+    stringConversion(file_contents)
+
+if __name__ == "__main__":
+    main()
