db584c92bdf039b4be5854ed8dca4278d64f345c
Dakkinbyte <fantauzzo94@gmail.com>
2024-04-01 20:03:58 -0400

    New Changes

--- main.py
+++ main.py
@@ -15,13 +15,9 @@ def letterCounter(text):
     d = {}
     lowerCasedString = text.lower()
     for character in lowerCasedString: 
-        # convert the character to lowercase ( lowered_string = input.lower() ) 
-        # if the character is already in the directory
-            # increase its count by 1
-        # else add the character to the dir with a count of 1 
         if character not in d:
             d[character] = 1
-        else
+        else:
             d[character] += 1 
 
     
