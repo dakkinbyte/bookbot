c913af86323d1e2ad7457a67d43b30d1c1512ded
Dakkinbyte <fantauzzo94@gmail.com>
2024-04-01 20:39:52 -0400

    Updated Main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def main():
     file_contents = read_file('books/frankenstein.txt')
     #print_file(file_contents)
-    #stringConversion(file_contents)
+    stringConversion(file_contents)
     letterCounter(file_contents)
 
 def print_file(file_path):
@@ -11,20 +11,33 @@ def read_file(file_path):
     with open(file_path, 'r') as file:
         return file.read()
 
+def sortOn(data):
+    return data["count"]
+
+def listCreation(data):
+    char_list = []
+    for char, count in data.items():
+        char_list.append({"char": char, "count": count})
+    char_list.sort(key=sortOn, reverse=True)
+    return char_list
+
 def letterCounter(text):
     d = {}
     lowerCasedString = text.lower()#.split() - Didnt know you could do this. 
     for character in lowerCasedString: 
-        if character not in d:
-            d[character] = 1
-        else:
-            d[character] += 1 
-    print(d)
+        if character.isalpha(): # check to amke sure the character is alphabetical
+            if character not in d:
+                d[character] = 1
+            else:
+                d[character] += 1 
+    sorted_characters = listCreation(d)
+    for item in sorted_characters:
+        print(f"The '{item['char']}' character was found {item['count']} times")
+    #print(f"DEBUG:", sorted_characters)
 
 def stringConversion(text):
-    words = input_string.split()
+    words = text.split()
     word_count = len(words)
-    return len(word_count)
     print("Word count: ", word_count)
 
 main()
