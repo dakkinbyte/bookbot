a4bd2065de14dabe007ee9f6aa91e58eb7c3cc90
Dakkinbyte <fantauzzo94@gmail.com>
2024-04-01 20:03:21 -0400

    New Changes

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def main():
     file_contents = read_file('books/frankenstein.txt')
-    print_file(file_contents)
-    stringConversion(file_contents)
+    #print_file(file_contents)
+    #stringConversion(file_contents)
+    letterCounter(file_contents)
 
 def print_file(file_path):
         print(file_path)
@@ -10,7 +11,22 @@ def read_file(file_path):
     with open(file_path, 'r') as file:
         return file.read()
 
-def stringConversion(input_string):
+def letterCounter(text):
+    d = {}
+    lowerCasedString = text.lower()
+    for character in lowerCasedString: 
+        # convert the character to lowercase ( lowered_string = input.lower() ) 
+        # if the character is already in the directory
+            # increase its count by 1
+        # else add the character to the dir with a count of 1 
+        if character not in d:
+            d[character] = 1
+        else
+            d[character] += 1 
+
+    
+
+def stringConversion(text):
     words = input_string.split()
     word_count = len(words)
     return len(word_count)
